56b8279e2093aa6e34777697aea09bdcd43b2700
dpankova <jet-engine@yandex.ru>
2019-06-19 19:18:53 -0400

    Use only max charge string

--- scripts/ExtractMQString.py
+++ scripts/ExtractMQString.py
@@ -116,13 +116,13 @@ def GetWaveform(frame):
     event_info['tau_direction']= (float(tau.dir.zenith),float(tau.dir.azimuth))
     event_info['tau_time']=(float(tau.time),float(frame["DP_Tau_End"].time))
     event_info['tau_length']=float(tau.length)
-    event_info['st_index']=len(data)
+    event_info['index']=len(data)
        
 
     wf_map = frame["CalibratedWaveformsHLCATWD"]
     omkeys = wf_map.keys()
 
-    strings = []
+
     strings_set = set()
     string_keys = {}
     for omkey in omkeys:
@@ -133,67 +133,82 @@ def GetWaveform(frame):
             string_keys[omkey.string].append(omkey)
     
     sort_st_charge = []
-    
+
     for string_num in string_keys:
         string_q = 0
-        im_data = []
-        doms = []
         for omkey in string_keys[string_num]:
             if not omkey in pulses:
                 continue        
 
             qs = pulses[omkey]
             qdom = sum(i.charge for i in qs)
-            dpos = geometry[omkey].position
             string_q = string_q + qdom
+        sort_st_charge.append([string_q,string_num])
+
+    sort_st_charge.sort(key=lambda x: x[0])
+    max_q_st = sort_st_charge[-1][1]
+    max_q = sort_st_charge[-1][0]
+#    print max_q
+    if max_q < 600:
+        return False
+
+    im_data = []    
+    doms = []
+
+    for omkey in string_keys[max_q_st]:
+        if not omkey in pulses:
+            continue        
         
-            dom = {}
-            dom['key'] = (int(omkey.string),int(omkey.om))
-            dom['qdom'] = qdom
-            dom['dom_position'] = (float(dpos.x),float(dpos.y),float(dpos.z))
-            
-            has_data = False
-            for wf in wf_map[omkey]:
-                if wf.status == 0 and wf.source_index == 0:
-                    has_data = True
-                    wf_vect = list(wf.waveform)
-                    for i, bin_val in enumerate(wf_vect):
-                        sample=[]
-                        sample.append(float(dpos.z))
-                        sample.append(float((wf.time+wf.bin_width*i)*v))
-                        sample.append(float(bin_val*10**12))
-                        im_data.append(np.array(sample))
+        qs = pulses[omkey]
+        dpos = geometry[omkey].position
+ 
 
-            if has_data == True:
+        dom = {}
+        dom['key'] = (int(omkey.string),int(omkey.om))
+        dom['qdom'] = qdom
+        dom['dom_position'] = (float(dpos.x),float(dpos.y),float(dpos.z))
+
+
+     
+        for wf in wf_map[omkey]:
+            if wf.status == 0 and wf.source_index == 0:
                 doms.append(dom)
-
-        if string_q > 600:
-            string = {}
-            string["number"] = string_num
-            string["doms"] = np.array(doms)
-            string["charge"] = string_q
-            string["index"] = len(data)
-            strings.append(string)
-            im_data = np.array(im_data)
-            dom_z = np.unique(im_data[:,0])
-            n_doms = len(dom_z)
-            z_min = dom_z[0]
-            z_max = dom_z[-1]
-            im_data = im_data[im_data[:, 1].argsort()]
-            width = 3.3333
-            x_min = im_data[:,1][0]
-            x_max = im_data[:,1][-1]
-            x_bin = (x_max-x_min)/width+1
-            
-            im, xed, yed = np.histogram2d(im_data[:,1],im_data[:,0],weights=im_data[:,2],bins=[x_bin,n_doms], range=[[x_min,x_max],[z_min,z_max]])
-            data.append(im)
+                wf_vect = list(wf.waveform)
+                for i, bin_val in enumerate(wf_vect):
+                    sample=[]
+                    sample.append(float(dpos.z))
+                    sample.append(float((wf.time+wf.bin_width*i)*v))
+                    sample.append(float(bin_val*10**12))
+                    im_data.append(np.array(sample))
 
 
+     
 
-    if len(strings)>0: 
-        event_info['strings'] = strings
-        event_info['end_index'] = len(data)-1
-        info.append(event_info)
+    string = {}
+    string["number"] = max_q_st
+    string["doms"] = np.array(doms)
+    string["charge"] = max_q
+        
+    im_data = np.array(im_data)
+    dom_z = np.unique(im_data[:,0])
+    n_doms = len(dom_z)
+    z_min = dom_z[0]
+    z_max = dom_z[-1]
+    im_data = im_data[im_data[:, 1].argsort()]
+    width = 3.3333
+    x_min = im_data[:,1][0]
+    x_max = im_data[:,1][-1]
+    x_bin = (x_max-x_min)/width+1
+        
+    im, xed, yed = np.histogram2d(im_data[:,1],im_data[:,0],weights=im_data[:,2],bins=[x_bin,n_doms], range=[[x_min,x_max],[z_min,z_max]])
+
+    data.append(im)
+
+
+
+
+    event_info['strings'] = string
+    info.append(event_info)
 
 
 #    print info
@@ -237,3 +252,5 @@ output_1.close()
 print "saved info"
 np.save(name+"_data"+".npy",data)
 print "finished"
+#print data
+#print info
